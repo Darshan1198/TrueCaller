bc8228e19bffa5a02e74e8a397a4d864b9635755
Darshan1198 <darshandarshi39@gmail.com>
2023-12-31 16:36:18 +0530

    Contact service

--- src/main/java/com/truecaller/Services/ContactService.java
+++ src/main/java/com/truecaller/Services/ContactService.java
@@ -1,4 +1,24 @@
 package com.truecaller.Services;
 
-public class ContactService {
-}
+import com.truecaller.models.constants.Contact;
+import com.truecaller.models.constants.ContactTypes;
+public interface ContactService {
+
+        // Add a new contact
+        Contact addContact(String phoneNumber, ContactTypes contactType);
+
+        // Block a contact
+        void blockContact(Long contactId);
+
+        // Unblock a contact
+        void unblockContact(Long contactId);
+
+        // Report a contact as spam
+        void reportSpam(Long contactId);
+
+        // Identify a caller
+        void identifyCaller(Long contactId, String callerName);
+
+        // Add a contact to the blacklist
+        void blackListContact(Long contactId);
+    }
---
+++ src/main/java/com/truecaller/Services/ContactServiceImpl.java
@@ -0,0 +1,4 @@
+package com.truecaller.Services;
+
+public class ContactServiceImpl {
+}
